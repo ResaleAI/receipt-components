daf3af60bfbc7644c66062c29b45e37f5d24416a
Zavier Miller <zavierjmiller@gmail.com>
2023-12-27 01:52:41 -0500

    make browser and node image package export consistent

--- packages/nodes/receipt-image-node/src/main.ts
+++ packages/nodes/receipt-image-node/src/main.ts
@@ -2,16 +2,14 @@ import { RCNodePlugin } from '@resaleai/receipt-plugin';
 import { buildImageNode, escPosRenderer, htmlRenderer } from './image';
 import { ImageNodeProps } from './image/types';
 
-const imagePlugin: [RCNodePlugin<ImageNodeProps>] = [
-  {
-    name: 'image',
-    aliases: ['img'],
-    buildNode: buildImageNode,
-    renderers: {
-      escpos: escPosRenderer,
-      html: htmlRenderer,
-    },
+const imagePlugin: RCNodePlugin<ImageNodeProps> = {
+  name: 'image',
+  aliases: ['img'],
+  buildNode: buildImageNode,
+  renderers: {
+    escpos: escPosRenderer,
+    html: htmlRenderer,
   },
-];
+};
 
 export default imagePlugin;
